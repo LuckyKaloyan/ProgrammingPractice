4687526a69802f17d06a3967050b21f7524aa388
LuckyKaloyan <luckykaloyan@gmail.com>
2023-09-08 17:39:32 +0300

    Update

---
+++ SoftUniJava/src/TestingTrash/HelloWorld.java
@@ -0,0 +1,20 @@
+package TestingTrash;
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int input = Integer.parseInt(scanner.nextLine());
+
+
+        for(int i=0; i<input; i++){
+
+        for(int j=0; j<input; j++){
+            System.out.print("  HW");
+        }
+            System.out.println();
+
+        }
+    }
+}
