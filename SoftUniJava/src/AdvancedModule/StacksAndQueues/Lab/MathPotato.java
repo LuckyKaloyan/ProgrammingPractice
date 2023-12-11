6631df9bf45ba637b90746efafb7617916f3a709
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-11 15:26:45 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/StacksAndQueues/Lab/MathPotato.java
@@ -0,0 +1,43 @@
+package AdvancedModule.StacksAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class MathPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] kids = scanner.nextLine().split(" ");
+        int nthToss = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        Collections.addAll(queue, kids);
+        int cycle = 1;
+
+        while (queue.size() > 1) {
+            for (int i = 1; i < nthToss; i++) {
+                queue.offer(queue.poll());
+            }
+
+            if (isPrime(cycle)) {
+                System.out.println("Prime " + queue.peek());
+            } else {
+                System.out.println("Removed " + queue.poll());
+            }
+
+            cycle++;
+        }
+
+        System.out.println("Last is " + queue.peek());
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
