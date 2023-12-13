fca34132ee109e3bc718268d8ea8dac1fcd56637
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-13 11:14:38 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/StacksAndQueues/Excercises/MaximumElement.java
@@ -0,0 +1,22 @@
+package AdvancedModule.StacksAndQueues.Excercises;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+public class MaximumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int commandsNumber = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for(int i=0; i<commandsNumber; i++){
+
+
+
+
+            
+        }
+
+
+    }
+}
