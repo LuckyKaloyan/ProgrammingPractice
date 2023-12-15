120e16bc377e184f93b5fe86d348a562324d3c72
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-15 10:55:25 +0200

    Update

--- SoftUniJava/src/AdvancedModule/StacksAndQueues/Excercises/BalancedParentheses.java
+++ SoftUniJava/src/AdvancedModule/StacksAndQueues/Excercises/BalancedParentheses.java
@@ -7,5 +7,33 @@ public class BalancedParentheses {
 
         String sequence = sc.nextLine();
         ArrayDeque<Character> stack = new ArrayDeque<>();
+        boolean areBalanced = true;
+        for (int i = 0; i < sequence.length(); i++) {
+            char current = sequence.charAt(i);
+            if (current == '[' || current == '{' || current == '(') {
+                stack.push(current);
+            } else {
+                if (stack.isEmpty()) {
+                    areBalanced = false;
+                    break;
+                }
+                char topElement = stack.pop();
+                if (current == ']' && topElement != '[') {
+                    areBalanced = false;
+                    break;
+                } else if (current == '}' && topElement != '{') {
+                    areBalanced = false;
+                    break;
+                } else if (current == ')' && topElement != '(') {
+                    areBalanced = false;
+                    break;
+                }
+            }
+        }
+        if (areBalanced) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
