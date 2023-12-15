5bd8b96acb1bc047f349b6de11aa9c65e75bf2ab
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-15 09:45:44 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/StacksAndQueues/Excercises/BalancedParentheses.java
@@ -0,0 +1,11 @@
+package AdvancedModule.StacksAndQueues.Excercises;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String sequence = sc.nextLine();
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+    }
+}
