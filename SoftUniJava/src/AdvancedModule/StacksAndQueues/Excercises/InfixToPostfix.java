1ba0d5175f69814987dc23a849a2c5ee3cd54f36
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-17 18:19:38 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/StacksAndQueues/Excercises/InfixToPostfix.java
@@ -0,0 +1,14 @@
+package AdvancedModule.StacksAndQueues.Excercises;
+import java.util.Scanner;
+public class InfixToPostfix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+    }
+}
---
+++ SoftUniJava/src/AdvancedModule/StacksAndQueues/Excercises/PoisonousPlants.java
@@ -0,0 +1,15 @@
+package AdvancedModule.StacksAndQueues.Excercises;
+import java.util.Scanner;
+public class PoisonousPlants {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+        
+
+    }
+
+}
