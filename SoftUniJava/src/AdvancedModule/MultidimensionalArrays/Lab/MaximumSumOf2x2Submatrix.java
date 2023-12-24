10bc72ad07b7d7c6d85ec8e9c187ce6fbd45dce2
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-24 19:11:32 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/MultidimensionalArrays/Lab/MaximumSumOf2x2Submatrix.java
@@ -0,0 +1,15 @@
+package AdvancedModule.MultidimensionalArrays.Lab;
+import java.util.Scanner;
+public class MaximumSumOf2x2Submatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+
+    }
+}
---
+++ SoftUniJava/src/AdvancedModule/MultidimensionalArrays/Lab/PrintDiagonalsOfSquareMatrix.java
@@ -0,0 +1,15 @@
+package AdvancedModule.MultidimensionalArrays.Lab;
+import java.util.Scanner;
+public class PrintDiagonalsOfSquareMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+
+    }
+}
