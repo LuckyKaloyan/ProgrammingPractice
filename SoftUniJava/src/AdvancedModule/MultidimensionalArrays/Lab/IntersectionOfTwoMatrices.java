dfb938fb9a31dccba065a43b85ae764253184073
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-24 19:10:11 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/MultidimensionalArrays/Lab/IntersectionOfTwoMatrices.java
@@ -0,0 +1,15 @@
+package AdvancedModule.MultidimensionalArrays.Lab;
+import java.util.Scanner;
+public class IntersectionOfTwoMatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+
+    }
+}
---
+++ SoftUniJava/src/AdvancedModule/MultidimensionalArrays/Lab/SumMatrixElements.java
@@ -0,0 +1,15 @@
+package AdvancedModule.MultidimensionalArrays.Lab;
+import java.util.Scanner;
+public class SumMatrixElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+
+    }
+}
