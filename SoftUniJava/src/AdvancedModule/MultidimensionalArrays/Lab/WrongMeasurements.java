b6a478aca079787250482c58dcfa66f843014a4e
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-25 21:55:10 +0200

    Update

--- SoftUniJava/src/AdvancedModule/MultidimensionalArrays/Lab/WrongMeasurements.java
+++ SoftUniJava/src/AdvancedModule/MultidimensionalArrays/Lab/WrongMeasurements.java
@@ -2,5 +2,8 @@ package AdvancedModule.MultidimensionalArrays.Lab;
 public class WrongMeasurements {
     public static void main(String[] args) {
         System.out.println("Martin Paunov e mnogo dobyr lektor, obache taq zadacha e napravena za autisti :)))");
+        System.out.println("Nqmam ideq kakvo mu e v glavata, obache tova uslovie :D :D :D ne otgovarq na nishto");
+        System.out.println("Daje primera ... LOL... LOL... Ako nqkoi, nqkyde, nqkoga prochete tova...");
+        System.out.println(":)");
     }
 }
