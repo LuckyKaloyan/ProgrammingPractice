01ed56513b99b863fcd3cb99952fa2de5df0e8e0
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-25 09:14:59 +0200

    Update

--- SoftUniJava/src/AdvancedModule/MultidimensionalArrays/Lab/CompareMatrices.java
+++ SoftUniJava/src/AdvancedModule/MultidimensionalArrays/Lab/CompareMatrices.java
@@ -9,13 +9,14 @@ public class CompareMatrices {
         for(int i=0; i<splitted[0]; i++){
             arreiche[i]=Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
+
         int[] splitted2 = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[][] arreiche2 = new int[splitted2[0]][splitted2[1]];
-        for(int i=0; i<splitted[0]; i++){
+        for(int i=0; i<splitted2[0]; i++){
             arreiche2[i]=Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
         boolean isWrong = false;
-        if(arreiche.length!=arreiche2.length){
+        if(arreiche.length!=arreiche2.length || arreiche[0].length!=arreiche2[0].length){
             isWrong = true;
         }else{
             for(int i=0; i<splitted[0]; i++){
