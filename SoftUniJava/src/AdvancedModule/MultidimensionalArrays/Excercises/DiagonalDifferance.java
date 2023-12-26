7c0bc5ca2a4ace59184e1b82a06d7759f496fd31
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-26 09:11:51 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/MultidimensionalArrays/Excercises/DiagonalDifferance.java
@@ -0,0 +1,8 @@
+package AdvancedModule.MultidimensionalArrays.Excercises;
+import java.util.Scanner;
+public class DiagonalDifferance {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+}
