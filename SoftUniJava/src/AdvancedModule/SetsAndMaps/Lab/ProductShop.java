a128ef9767328acab7eac262556ee73d69eefb6d
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-30 21:23:07 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/SetsAndMaps/Lab/ProductShop.java
@@ -0,0 +1,25 @@
+package AdvancedModule.SetsAndMaps.Lab;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class ProductShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+      TreeMap<String, TreeMap<String,Double>> mapche = new TreeMap<>();
+
+      String input = scanner.nextLine();
+      while(!input.equals("Revision")){
+          
+
+
+
+
+          input = scanner.nextLine();
+      }
+
+
+
+    }
+}
