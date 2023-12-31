7b56b27d0461a6cf2c8a2df1849e358b4f3b7da7
LuckyKaloyan <luckykaloyan@gmail.com>
2023-12-31 17:52:44 +0200

    Update

--- SoftUniJava/src/AdvancedModule/SetsAndMaps/Lab/ProductShop.java
+++ SoftUniJava/src/AdvancedModule/SetsAndMaps/Lab/ProductShop.java
@@ -1,5 +1,7 @@
 package AdvancedModule.SetsAndMaps.Lab;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -7,19 +9,30 @@ public class ProductShop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-      TreeMap<String, TreeMap<String,Double>> mapche = new TreeMap<>();
+      TreeMap<String, LinkedHashMap<String,Double>> mapche = new TreeMap<>();
 
       String input = scanner.nextLine();
       while(!input.equals("Revision")){
-          
+          String[] splitted = input.split(", ");
+          String shopname = splitted[0];
+          String productname = splitted[1];
+          double price = Double.parseDouble(splitted[2]);
 
-
-
-
+          if(!mapche.containsKey(shopname)){
+              LinkedHashMap<String,Double> products = new LinkedHashMap<>();
+              products.put(productname,price);
+              mapche.put(shopname,products);
+          }else{
+              mapche.get(shopname).put(productname,price);
+          }
           input = scanner.nextLine();
       }
-
-
-
+      for(Map.Entry<String,LinkedHashMap<String,Double>>entry: mapche.entrySet()){
+          System.out.println(entry.getKey()+"->");
+          for(Map.Entry<String,Double> entry1:entry.getValue().entrySet()){
+              System.out.printf("Product: %s, Price: %.1f",entry1.getKey(),entry1.getValue());
+              System.out.println();
+          }
+      }
     }
 }
