cbf7738bbee157dad26df778a4cf01bf676123b5
Калоян Димитров <luckykaloyan@gmail.com>
2024-01-02 08:25:31 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/SetsAndMaps/Excercises/PeriodicTable.java
@@ -0,0 +1,12 @@
+package AdvancedModule.SetsAndMaps.Excercises;
+import java.util.Scanner;
+public class PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+    }
+}
