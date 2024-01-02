fd66c289528a9c6aaf2209da1e75f65cb64c2436
Калоян Димитров <luckykaloyan@gmail.com>
2024-01-02 10:15:28 +0200

    Update

--- SoftUniJava/src/AdvancedModule/SetsAndMaps/Excercises/Phonebook.java
+++ SoftUniJava/src/AdvancedModule/SetsAndMaps/Excercises/Phonebook.java
@@ -1,5 +1,4 @@
 package AdvancedModule.SetsAndMaps.Excercises;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -7,15 +6,11 @@ import java.util.Scanner;
 public class Phonebook {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
          String input = scanner.nextLine();
           Map<String,String> mapche = new HashMap<>();
          while(!input.equals("search")){
              String[] splitted = input.split("-");
              mapche.put(splitted[0],splitted[1]);
-
-
-
              input = scanner.nextLine();
          }
          while(!input.equals("stop")){
@@ -24,8 +19,6 @@ public class Phonebook {
              }else{
                  System.out.println("Contact "+input+" does not                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       ");
              }
-
-
              input = scanner.nextLine();
          }
     }
