ca3abc2204cec7873017a894a70aeeb21a547cf9
Калоян Димитров <luckykaloyan@gmail.com>
2024-01-05 09:06:10 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/StreamFilesDirectories/Lab/WriteFile.java
@@ -0,0 +1,32 @@
+package AdvancedModule.StreamFilesDirectories.Lab;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteFile {
+    public static void main(String[] args) throws IOException {
+        FileInputStream kiufte = new FileInputStream("C:\\Users\\User\\Desktop\\Материали\\input.txt");
+
+FileOutputStream kebabche = new FileOutputStream("C:\\Users\\User\\Desktop\\Материали\\output.txt");
+
+
+List<String> listche = new ArrayList<>();
+listche.add(",");
+listche.add("!");
+listche.add(".");
+listche.add("?");
+
+        int oneByte = kiufte.read();
+
+while(oneByte>=0){
+
+    if(!listche.contains(String.valueOf((char)oneByte))) {
+        kebabche.write((char)oneByte);
+    }
+    oneByte = kiufte.read();
+}
+    }
+}
