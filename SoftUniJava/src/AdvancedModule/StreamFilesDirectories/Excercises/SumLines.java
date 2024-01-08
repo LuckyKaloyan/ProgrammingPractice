3a949f63985e9a1072b49fe7dab6a70eda12ad46
Калоян Димитров <luckykaloyan@gmail.com>
2024-01-08 12:00:52 +0200

    Update

---
+++ SoftUniJava/src/AdvancedModule/StreamFilesDirectories/Excercises/SumLines.java
@@ -0,0 +1,24 @@
+package AdvancedModule.StreamFilesDirectories.Excercises;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SumLines {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader kiufte = new BufferedReader(new FileReader("C:\\Users\\User\\Desktop\\Resource\\input.txt"));
+
+        
+        int bytes = kiufte.readLine();
+
+
+        while(bytes>0){
+
+        }
+
+
+
+
+    }
+}
