9322f6b69d3ca6c9a584e06b4cef8bcf44570847
Калоян Димитров <luckykaloyan@gmail.com>
2024-01-09 09:23:10 +0200

    Update

--- SoftUniJava/src/AdvancedModule/StreamFilesDirectories/Excercises/ALLCAPITALS.java
+++ SoftUniJava/src/AdvancedModule/StreamFilesDirectories/Excercises/ALLCAPITALS.java
@@ -7,13 +7,27 @@ import java.io.IOException;
 public class ALLCAPITALS {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader kiufte = new BufferedReader(new FileReader("C:\\Users\\User\\Desktop\\Resource\\input.txt"));
+        BufferedReader kiufte = new BufferedReader(new FileReader("C:\\Users\\User\\Desktop\\Resource\\Exercises Resources\\input.txt"));
         String bytes = kiufte.readLine();
-        while(bytes!=null){
-            System.out.println(bytes.toUpperCase());
+        int otherSymbols = 0;
+        int vowels = 0;
+        int punctuationMarks = 0;
 
+        while(bytes!=null){
+            for(int i=0; i<bytes.length(); i++){
+                if(bytes.charAt(i)!=' '){
+                    if(bytes.charAt(i)=='a' || bytes.charAt(i)=='o' || bytes.charAt(i)=='i' || bytes.charAt(i)=='e' || bytes.charAt(i)=='u'){
+                        vowels++;
+                    }else if(bytes.charAt(i)=='!' || bytes.charAt(i)==',' || bytes.charAt(i)=='.' || bytes.charAt(i)=='?'){
+                        punctuationMarks++;
+                    }else{
+                        otherSymbols++;
+                    }
+                }
+            }
             bytes = kiufte.readLine();
         }
+        System.out.printf("Vowels: %d%nOther symbols: %d%nPunctuation: %d",vowels,otherSymbols,punctuationMarks);
 
 
 
