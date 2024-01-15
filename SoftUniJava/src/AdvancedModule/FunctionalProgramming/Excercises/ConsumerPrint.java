bdcb73a9f720c81453ee7a2e3a229247d064725a
Калоян Димитров <luckykaloyan@gmail.com>
2024-01-15 10:32:35 +0200

    Update

--- SoftUniJava/src/AdvancedModule/FunctionalProgramming/Excercises/ConsumerPrint.java
+++ SoftUniJava/src/AdvancedModule/FunctionalProgramming/Excercises/ConsumerPrint.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class ConsumerPrint {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         Arrays.stream(scanner.nextLine().split(" ")).forEach(System.out::println);
     }
 }
