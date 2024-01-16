251b67ddee44d8d6874bc3e7301bcdcf6f4653dc
LuckyKaloyan <luckykaloyan@gmail.com>
2024-01-16 15:53:27 +0200

    Update

--- SoftUniJava/src/AdvancedModule/FunctionalProgramming/Excercises/PredicateForNames.java
+++ SoftUniJava/src/AdvancedModule/FunctionalProgramming/Excercises/PredicateForNames.java
@@ -1,14 +1,8 @@
 package AdvancedModule.FunctionalProgramming.Excercises;
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 3062c12b3f36652b09a480d11a812aa17b801119
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.function.Predicate;
 
-<<<<<<< HEAD
 public class PredicateForNames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -21,36 +15,5 @@ public class PredicateForNames {
        for(String lesh:kebabta){
            System.out.println(lesh);
        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
-=======
-
-public class PredicateForNames {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int i = Integer.parseInt(scanner.nextLine());
-        String kebab = scanner.nextLine();
-        Predicate<String> opa = s -> s.length()<=i;
-        String[] kebabta = Arrays.stream(kebab.split(" ")).filter(opa).toArray(String[]::new);
-
-        for(String lesh:kebabta){
-            System.out.println(lesh);
-        }
-    }
-}
->>>>>>> 3062c12b3f36652b09a480d11a812aa17b801119
