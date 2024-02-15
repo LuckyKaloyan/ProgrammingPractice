c7a10413a03fed37e78460cdb74442d1c9c5bc34
LuckyKaloyan <luckykaloyan@gmail.com>
2024-02-15 20:01:41 +0200

    Update

--- SoftUniJava/src/AdvancedModule/StacksAndQueues/Lab/MathPotato.java
+++ SoftUniJava/src/AdvancedModule/StacksAndQueues/Lab/MathPotato.java
@@ -1,40 +1,41 @@
 package AdvancedModule.StacksAndQueues.Lab;
 
-import java.util.ArrayDeque;
-import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
+
 public class MathPotato {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] kids = scanner.nextLine().split(" ");
-        int nthToss = Integer.parseInt(scanner.nextLine());
-        ArrayDeque<String> queue = new ArrayDeque<>();
-        Collections.addAll(queue, kids);
+        String[] names = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        PriorityQueue<String> kid = new PriorityQueue<>();
+        for (String name : names) {
+            kid.offer(name);
+        }
+
         int cycle = 1;
-
-        while (queue.size() > 1) {
-            for (int i = 1; i < nthToss; i++) {
-                queue.offer(queue.poll());
+        while (kid.size() > 1) {
+            for (int i = 1; i < n; i++) {
+                kid.offer(kid.poll());
             }
-            String[] names = scanner.nextLine().split(" ");
-
-            if (isPrime(cycle)) {
-                System.out.println("Prime " + queue.peek());
+            if (!isPrimeNumber(cycle)) {
+                System.out.printf("Removed %s%n", kid.poll());
             } else {
-                System.out.println("Removed " + queue.poll());
+                System.out.printf("Prime %s%n", kid.peek());
             }
-
             cycle++;
         }
 
-        System.out.println("Last is " + queue.peek());
+        System.out.printf("Last is %s%n", kid.poll());
     }
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
+
+    private static boolean isPrimeNumber(int cycle) {
+        if (cycle <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
+        for (int i = 2; i <= Math.sqrt(cycle); i++) {
+            if (cycle % i == 0) {
                 return false;
             }
         }
